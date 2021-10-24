bf338f1d19829a8e603922bac507b60e292349f9
Coromoto León <cleon@ull.edu.es>
2021-10-24 21:05:24 +0000

    Afirmaciones (asserts) para el producto por un escalar

--- test/tc_point.rb
+++ test/tc_point.rb
@@ -10,4 +10,8 @@ class TestPoint < Test::Unit::TestCase
      # Comprobar getter y 
      assert_equal(0, Point.new(1,0).y)
   end
+
+  def test_simple_escalar
+    assert_equal("(5,5)", (Point.new(1,1)*5).to_s)
+  end
 end
