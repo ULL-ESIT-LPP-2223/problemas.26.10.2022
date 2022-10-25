da177ab904cd66089a6582210fec2d65cd20be57
Coromoto Leon <cleon@ull.edu.es>
2022-10-25 17:50:10 +0000

    Se implemento el initialize los getters y to_s
    rspec spec/punto_spec.rb
    ...
    
    Finished in 0.00609 seconds (files took 0.14217 seconds to load)
    3 examples, 0 failures

--- spec/punto_spec.rb
+++ spec/punto_spec.rb
@@ -5,13 +5,13 @@ describe Punto do
   # Describe la prueba
   it "Se instancia un punto con coordenadas x, y" do
     # Define la expectativa
-    expect(Point.new(0,0)).not_to eq(nil) #matcher
+    expect(Punto.new(0,0)).not_to eq(nil) #matcher
   end
   it "Se debe poder acceder a las coordenadas con los getters" do
-    expect(Point.new(0,0).x).to eq(0)
-    expect(Point.new(0,0).y).to eq(0)
+    expect(Punto.new(0,0).x).to eq(0)
+    expect(Punto.new(0,0).y).to eq(0)
   end
   it "El formato del punto sera (x,y)" do
-    expect(Point.new(0,0).to_s).to eq("(0,0)")
+    expect(Punto.new(0,0).to_s).to eq("(0,0)")
   end
 end
