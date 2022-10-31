0250366a3d06e2604674de9fc7f506f657f6b2f3
Coromoto Leon <cleon@ull.edu.es>
2022-10-31 07:25:27 +0000

    Añadido el before :all

--- spec/punto_spec.rb
+++ spec/punto_spec.rb
@@ -2,6 +2,10 @@ require 'punto'
 
 # Permite agrupar pruebas
 describe Punto do
+  before :all do
+    @p0 = Punto.new(0,0)
+    @p1 = Punto.new(1,1)
+  end
   context "Atributos, getters y to_string" do
     # Describe la prueba
     it "Se instancia un punto con coordenadas x, y" do
@@ -11,9 +15,12 @@ describe Punto do
     it "Se debe poder acceder a las coordenadas con los getters" do
       expect(Punto.new(0,0).x).to eq(0)
       expect(Punto.new(0,0).y).to eq(0)
+      expect(@p0.x).to eq(0)
+      expect(@p0.y).to eq(0)
     end
     it "El formato del punto sera (x,y)" do
       expect(Punto.new(0,0).to_s).to eq("(0,0)")
+      expect(@p0.to_s).to eq("(0,0)")
     end
   end
   context "Métodos de intstancia" do
@@ -22,6 +29,8 @@ describe Punto do
         p2 = Punto.new(1,1)*5
         expect(p2.x).to eq(5)
         expect(p2.y).to eq(5)
+        expect((@p1*5).x).to eq(5)
+        expect((@p1*5).y).to eq(5)
       end
       it "Si se multiplica por -1 se obtiene el opuesto" do
         p2 = Punto.new(1,1)*-1
@@ -43,6 +52,7 @@ describe Punto do
        end
        it "Al sumar el origen y la unidad da la unidad - to_s" do
          expect((Punto.new(0,0) + Punto.new(1,1)).to_s).to eq("(1,1)")
+         expect((@p0 + @p1).to_s).to eq("(1,1)")
        end
     end
   end # Métodos de instancia
